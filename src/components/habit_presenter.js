1089a15d53a479ec73d705abfcf83ffbb5f622b5
dngwoodo <dngwoodo@gmail.com>
2021-12-29 21:05:46 +0900

    habit_presenter 테스트코드 리팩토링

--- src/components/habit_presenter.js
+++ src/components/habit_presenter.js
@@ -25,7 +25,7 @@ export default class HabitPresenter {
   decrement(habit, update) {
     this.habits = this.habits.map(item => {
       if (item.id === habit.id) {
-        const count = habit.count - 1;
+        const count = item.count - 1;
         return { ...habit, count: count < 0 ? 0 : count };
       }
 
--- src/components/tests/habit_presenter.test.js
+++ src/components/tests/habit_presenter.test.js
@@ -11,25 +11,23 @@ const HABITS = [
 ];
 
 describe('HabitPresenter', () => {
-  const update = jest.fn();
-  
+  let update;
   let habitPresenter;
-
+  
   beforeEach(() => {
     getId.mockImplementation(() => 100);
-
-    update.mockClear();
     
+    update = jest.fn();
     habitPresenter = new HabitPresenter(HABITS);
   })
 
-  it('returns habits', () => {
+  it('inits habits', () => {
     const habits = habitPresenter.getHabits();
 
     expect(habits).toEqual(HABITS);
   });
 
-  it('increases habit count', () => {
+  it('increments habit count and calls update', () => {
     habitPresenter.increment(HABITS[0], update);
 
     const habits = habitPresenter.getHabits();
@@ -39,23 +37,28 @@ describe('HabitPresenter', () => {
       { id: 2, name: 'Running', count: 0 },
       { id: 3, name: 'Coding', count: 1 },
     ]);
-    expect(update).toBeCalled();
+    expect(update).toBeCalledTimes(1);
+    expect(update).toBeCalledWith(habitPresenter.getHabits());
   });
 
-  it('decreases habit count', () => {
+  it('decrements habit count and calls update', () => {
     habitPresenter.decrement(HABITS[2], update);
 
-    const habits = habitPresenter.getHabits();
-
-    expect(habits).toEqual( [
-      { id: 1, name: 'Reading', count: 0 },
-      { id: 2, name: 'Running', count: 0 },
-      { id: 3, name: 'Coding', count: 0 },
-    ]);
-    expect(update).toBeCalled();
+    expect(habitPresenter.getHabits()[2].count).toBe(0);
+    expect(update).toBeCalledTimes(1);
+    expect(update).toBeCalledWith(habitPresenter.getHabits());
   });
 
-  it('delete habit', () => {
+  // "테스트를 의도적으로 실패할 수 있는지를 체크하자"!!
+  it('doesn\'t set count value below 0 when decrements', () => {
+    habitPresenter.decrement(HABITS[0], update);
+
+    expect(habitPresenter.getHabits()[0].count).toEqual(0);
+    expect(update).toBeCalledTimes(1);
+    expect(update).toBeCalledWith(habitPresenter.getHabits());
+  });
+
+  it('delete habit and calls update', () => {
     habitPresenter.delete(HABITS[0], update);
     const habits = habitPresenter.getHabits();
 
@@ -63,19 +66,21 @@ describe('HabitPresenter', () => {
       { id: 2, name: 'Running', count: 0 },
       { id: 3, name: 'Coding', count: 1 },
     ]);
-    expect(update).toBeCalled();
+    expect(update).toBeCalledTimes(1);
+    expect(update).toBeCalledWith(habitPresenter.getHabits());
   });
 
-  it('add habit', () => {
+  it('add habit and calls update', () => {
     habitPresenter.add('xxx', update);
 
     const habits = habitPresenter.getHabits();
 
     expect(habits).toEqual([...HABITS, { id: 100, name: 'xxx', count: 0}]);
-    expect(update).toBeCalled();
+    expect(update).toBeCalledTimes(1);
+    expect(update).toBeCalledWith(habitPresenter.getHabits());
   });
 
-  it('reset habit\'s count', () => {
+  it('reset habit\'s count and calls update', () => {
     habitPresenter.reset(update);
 
     const habits = habitPresenter.getHabits();
@@ -85,6 +90,7 @@ describe('HabitPresenter', () => {
       { id: 2, name: 'Running', count: 0 },
       { id: 3, name: 'Coding', count: 0 },
     ]);
-    expect(update).toBeCalled();
+    expect(update).toBeCalledTimes(1);
+    expect(update).toBeCalledWith(habitPresenter.getHabits());
   });
 });
